6d9a3798f2cdbe06e1d9daaf5cb9747b61ba4040
Deepak Aakula <deepakaakula@Deepaks-MacBook-Pro.local>
2018-11-05 17:22:17 -0500

    Login/Signup integrated
    could not append the code to the 'account' feature in nav bar. Need to work on the fragments.

--- android-app/app/src/main/java/com/sweng888/quicktrip/MainActivity.java
+++ android-app/app/src/main/java/com/sweng888/quicktrip/MainActivity.java
@@ -3,6 +3,8 @@ package com.sweng888.quicktrip;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -10,24 +12,31 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.Intent;
 
 import com.sweng888.quicktrip.fragment.PreferenceItemFragment;
 import com.sweng888.quicktrip.fragment.PreferencesFragment;
 import com.sweng888.quicktrip.model.UserPreference;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, PreferencesFragment.OnFragmentInteractionListener, PreferenceItemFragment.OnListFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, PreferencesFragment.OnFragmentInteractionListener, PreferenceItemFragment.OnListFragmentInteractionListener {
+
+    private DrawerLayout DrawerLayout;
+    private ActionBarDrawerToggle aToggle;
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -37,19 +46,20 @@ public class MainActivity extends AppCompatActivity
 //            }
 //        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -88,10 +98,17 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_preferences) {
 
         }
+        if (id == R.id.nav_account) {
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+            Intent intent = new Intent( MainActivity.this, LoginActivity.class);
+            getApplicationContext().startActivity(intent);
+
+        }
+
+        //DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
         return true;
+
     }
 
     @Override
@@ -103,4 +120,6 @@ public class MainActivity extends AppCompatActivity
     public void onListFragmentInteraction(UserPreference item) {
 
     }
+
+
 }
